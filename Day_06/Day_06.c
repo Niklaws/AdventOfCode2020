b768c9f85070ed3eaf9675c92907f2c0db9c33ee
Niklas <nik.42.lie@gmail.com>
2020-12-07 21:47:34 +0100

    fixed it

--- Day_06/Day_06.c
+++ Day_06/Day_06.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 #define NUM 456
+#define BUFSIZE 128
 
 int getInput(char input[NUM][BUFSIZE]);
+int partOne(char input[NUM][BUFSIZE]);
+int partTwo(char input[NUM][BUFSIZE]);
 
 int main(int argc, int **argv)
 {
-	char input[NUM][BUFSIZE]
+	char input[NUM][BUFSIZE];
+	getInput(input);
+	for(int i=0; i<NUM; i++)
+	{
+		printf("%s\n", input[i]);
+	}
+	partOne(input);
+	partTwo(input);
 	return 0;
 }
 
@@ -26,4 +37,49 @@ int getInput(char input[NUM][BUFSIZE])
 	}
 	fclose(file);
 	return 0;
+}
+
+int partOne(char input[NUM][BUFSIZE])
+{
+	int cnt = 0;
+	for(int i=0; i<NUM; i++)
+	{
+		for(int j=97; j<=122; j++)
+		{
+			if(strchr(input[i], j) != NULL)
+			{
+				cnt++;
+			}
+		}
+	}
+	printf("%d\n", cnt);
+}
+
+int partTwo(char input[NUM][BUFSIZE])
+{
+	int c;
+	int cnt = 0;
+	for(int i=0; i<5; i++)
+	{
+		char *t = strchr(input[i], '\n');
+		c = 0;
+		while(t != NULL)
+		{
+			t++;
+			c++;
+			t = strchr(t, '\n');
+		}
+
+		char temp[c][BUFSIZE];
+		t = input[i];
+		printf("c: %d\n", c);
+		for(int j=0; j<c; j++)
+		{
+			sscanf(t, "%s\n", temp[j]);
+			t = strchr(t, '\n');
+			t = t + 1;
+			printf("%d: %s|\n", j, temp[j]);
+		}
+	}
+	printf("%d\n", cnt);
 }
