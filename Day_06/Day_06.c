35baf5e2cd6678ffa42799722ecd4310e640e299
Niklas <nik.42.lie@gmail.com>
2020-12-07 21:57:03 +0100

    day 6 part 2 maybe done

--- Day_06/Day_06.c
+++ Day_06/Day_06.c
@@ -59,7 +59,7 @@ int partTwo(char input[NUM][BUFSIZE])
 {
 	int c;
 	int cnt = 0;
-	for(int i=0; i<5; i++)
+	for(int i=0; i<NUM; i++)
 	{
 		char *t = strchr(input[i], '\n');
 		c = 0;
@@ -79,6 +79,39 @@ int partTwo(char input[NUM][BUFSIZE])
 			t = strchr(t, '\n');
 			t = t + 1;
 			printf("%d: %s|\n", j, temp[j]);
+		}
+
+		int alph[26];
+		for(int j=97; j<=122; j++)
+		{
+			if(strchr(temp[0], j) != NULL)
+			{
+				alph[j-97] = 1;
+			}
+			else
+			{
+				alph[j-97] = 0;
+			}
+		}
+
+		for(int j=1; j<c; j++)
+		{
+			for(int k=97; k<=122; k++)
+			{
+				if(strchr(temp[j], k) != NULL && alph[k-97] == 1)
+				{
+					alph[k-97] = 1;
+				}
+				else
+				{
+					alph[k-97] = 0;
+				}
+			}
+		}
+
+		for(int j=0; j<26; j++)
+		{
+			cnt = cnt + alph[j];
 		}
 	}
 	printf("%d\n", cnt);
